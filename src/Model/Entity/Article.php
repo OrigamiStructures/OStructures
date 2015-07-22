f1de36f0f5243d5a63972822188c90787bb61eda
Jason Tempestini <jason@curlymedia.com>
2015-07-22 11:41:14 -0700

    Article bake

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Article Entity.
+ */
+class Article extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'title' => true,
+        'text' => true,
+        'type' => true,
+        'slug' => true,
+        'summary' => true,
+        'images' => true,
+    ];
+}
