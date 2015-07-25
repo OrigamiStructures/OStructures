2a3eff0069a6057c8ef83a53d9826580ed4ddb69
Don Drake <ddrake@dreamingmind.com>
2015-07-24 23:09:51 -0700

    Add entity interfaces

--- src/Model/Entity/Article.php
+++ src/Model/Entity/Article.php
@@ -2,14 +2,15 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake3xMarkdown\Model\Entity\Interfaces\MarkdownInterface;
+use Cake3xMarkdown\Model\Entity\Interfaces\GeshiInterface;
 
 /**
  * Article Entity.
  */
-class Article extends Entity
-{
-
-    /**
+class Article extends Entity implements MarkdownInterface, GeshiInterface {
+	
+	/**
      * Fields that can be mass assigned using newEntity() or patchEntity().
      *
      * @var array
@@ -22,4 +23,21 @@ class Article extends Entity
         'summary' => true,
         'images' => true,
     ];
+
+	public function markdownCacheConfig($options = NULL) {
+		return '_markdown_';
+	}
+
+	public function markdownCacheKey($options = NULL) {
+		return '_article' . $this->id . $this->modified->nice();
+	}
+
+	public function markdownCaching($options = NULL) {
+		return TRUE;
+	}
+
+	public function markdownSource($options = NULL) {
+		return $this->text;
+	}
+
 }
