27dd970d3310854004d31982b309f6b25a402def
Jason Tempestini <jason@curlymedia.com>
2015-08-12 14:42:55 -0700

    Have BlogArticles Edit pull all image types
    Images are grouped by:
    articleImages (containing all images connected to this article)
    linkedImages (containing all images linked to OTHER articles, but not this one)
    unlinkedImages (containing all images linked to NO articles)

--- src/Controller/BlogArticlesController.php
+++ src/Controller/BlogArticlesController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Controller\ArticlesController;
+use Cake\Collection\Collection;
+use Cake\Log\Log;
 
 /**
  * CakePHP BlogArticle
@@ -11,12 +13,22 @@ class BlogArticlesController extends ArticlesController {
 	
 	public function edit($id = null) {
 		$this->loadModel('Articles');
-        $article = $this->Articles->get($id, [
-            'contain' => ['Images', 'Topics']
-        ]);
+        try {
+            $article = $this->Articles->get($id, [
+                'contain' => ['Images', 'Topics']
+            ]);
+            $articleImages = new Collection($article->images);
+            $Images = $this->Articles->Images->find('all');
+            $Images->contain(['Articles']);
+            $unlinkedImages = $this->unlinkedImages($Images);
+            $linkedImages = $this->linkedImages($Images, $id);
+        } catch (Exception $exc) {
+            $article = $articleImages = $unlinkedImages = $linkedImages = array();
+            echo $exc->getTraceAsString();
+        }
+
         if ($this->request->is(['patch', 'post', 'put'])) {
             $article = $this->Articles->patchEntity($article, $this->request->data);
-//			debug($this->request->data);die;
             if ($this->Articles->save($article)) {
                 $this->Flash->success(__('The article has been saved.'));
 				if (!$this->request->data['continue']) {
@@ -26,9 +38,45 @@ class BlogArticlesController extends ArticlesController {
                 $this->Flash->error(__('The article could not be saved. Please, try again.'));
             }
         }
-        $this->set(compact('article'));
+        $this->set(compact('article', 'articleImages', 'unlinkedImages', 'linkedImages'));
         $this->set('_serialize', ['article']);
 	}
+    
+    /**
+     * Filter the provided $Images object to return those not linked to any article
+     * 
+     * @param object $Images
+     * @return object the unlinked images as a Collection Object
+     */
+    private function unlinkedImages($Images) {
+        return $Images->filter(function($image, $key){
+                return count($image->articles) == 0;
+            });
+    }
+    
+    /**
+     * Filter the provided $Images object to return those only linked to OTHER articles
+     * 
+     * @param object $Images
+     * @param string $id the current article id
+     * @return object the linked images as a Collection Object
+     */
+    private function linkedImages($Images, $id) {
+        $test_array = [];
+        return $Images->filter(function($image, $key) use ($id, &$test_array){
+            $test_array[$image->id] = [];
+            $imageId = $image->id;
+            $articles = New Collection($image->articles);
+            $articles = $articles->each(function($value, $key) use (&$test_array, $imageId){
+                $test_array[$imageId][$value->id] = $value->id;
+            });
+            if(in_array($id, $test_array[$imageId]) || empty($test_array[$imageId])){
+                return FALSE;
+            } else {
+                return TRUE;
+            }
+        });
+    }
 	
 	public function add() {
 		$this->loadModel('Articles');
