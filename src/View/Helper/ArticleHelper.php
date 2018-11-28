51fd42d002a15e3ada1ed2478c1686d4adc1a27f
Jason Tempestini <jason@curlymedia.com>
2018-11-28 13:55:29 -0800

    Remove questionable edit link security

--- src/View/Helper/ArticleHelper.php
+++ src/View/Helper/ArticleHelper.php
@@ -17,8 +17,12 @@ class ArticleHelper extends Helper {
 	public $helpers = ['Html'];
 
 	public function editLink($article) {
-		return preg_match('/^d(ev)*\./', strtolower($this->request->env('HTTP_HOST'))) ? 
-			$this->Html->link('Edit', ['action' => 'edit', $article->id]) . ' || ' :
-			'';
+        return $this->Html->link('Edit', ['action' => 'edit', $article->id]) . ' || ';
+//
+//        Likely bad security process removed JT 2018.11.28 1:54 PM
+//
+//        return preg_match('/^d(ev)*\./', strtolower($this->request->env('HTTP_HOST'))) ?
+//            $this->Html->link('Edit', ['action' => 'edit', $article->id]) . ' || ' :
+//			'';
 	}
 }
